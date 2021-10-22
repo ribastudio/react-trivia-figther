44e5f3191dcca1bbd95f09fb415b7c2c5f31f51f
Eicke Yumoto <eickeyumoto@gmail.com>
2021-10-22 19:16:25 -0300

    requisito 9: completo :rocket:

--- src/components/Gameplay.jsx
+++ src/components/Gameplay.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchTriviaQuestions } from '../services/TriviaAPI';
 import ButtonAnswer from './ButtonAnswer';
 import Timer from './Timer';
+import { stopInterval } from '../redux/actions';
 
 class Gameplay extends Component {
   constructor(props) {
@@ -15,6 +17,7 @@ class Gameplay extends Component {
 
     this.fechting = this.fechting.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.totalScore = this.totalScore.bind(this);
   }
 
   componentDidMount() {
@@ -28,15 +31,46 @@ class Gameplay extends Component {
     });
   }
 
-  handleClick() {
+  handleClick({ target }) {
     const buttons = document.querySelectorAll('button');
+    const { dispatchStopInterval, name, gravatarEmail } = this.props;
     buttons.forEach((button) => {
+      button.disabled = true;
       if (button.className === 'correct') {
         button.style.border = '3px solid rgb(6, 240, 15)';
       } else {
         button.style.border = '3px solid rgb(255, 0, 0)';
       }
     });
+    dispatchStopInterval();
+    const parcialScore = JSON.parse(localStorage.getItem('state'));
+    let { score } = parcialScore.player;
+    if (target.className === 'correct') {
+      score += this.totalScore();
+    }
+    localStorage.setItem('state', JSON.stringify({
+      player: {
+        name,
+        score,
+        gravatarEmail,
+      } }));
+  }
+
+  totalScore() {
+    const { timer } = this.props;
+    const { controller, results } = this.state;
+    const difficult = results[controller].difficulty;
+    const EASY = 1;
+    const MEDIUM = 2;
+    const HARD = 3;
+    const TEN = 10;
+    switch (difficult) {
+    case 'easy':
+      return TEN + (timer * EASY);
+    case 'medium':
+      return TEN + (timer * MEDIUM);
+    default: return TEN + (timer * HARD);
+    }
   }
 
   render() {
@@ -59,4 +93,21 @@ class Gameplay extends Component {
   }
 }
 
-export default connect(null)(Gameplay);
+Gameplay.propTypes = {
+  dispatchStopInterval: PropTypes.func.isRequired,
+  timer: PropTypes.number.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchStopInterval: () => dispatch(stopInterval()),
+});
+
+const mapStateToProps = (state) => ({
+  timer: state.triviaReducer.timer,
+  name: state.userReducer.name,
+  gravatarEmail: state.userReducer.email,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gameplay);
